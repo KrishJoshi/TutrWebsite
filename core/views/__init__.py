0fc228a9ea217ad09c3c0a1f4c1558047651e695
Joseph Kahihia <info@chrystalsolution.com>
2016-08-15 09:14:09 +0200

    django backend and angularjs frontend

--- core/views/__init__.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic.base import TemplateView
-
-
-class IndexView(TemplateView):
-    template_name = 'index.html'
-
-    def dispatch(self, *args, **kwargs):
-        return super(IndexView, self).dispatch(*args, **kwargs)
--- frontend/src/scripts/services/ParseServices.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('ParseServices', [])
-.factory('ParseSDK', function() {
-
-  // pro-tip: swap these keys out for PROD keys automatically on deploy using grunt-replace
-
-
-});
