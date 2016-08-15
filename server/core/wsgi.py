60c4b031d80118fe926675d369a4ee9a1d3b99d4
Joseph Kahihia <info@chrystalsolution.com>
2016-08-15 09:19:34 +0200

    django backend and angularjs frontend

--- server/core/wsgi.py
+++ server/core/wsgi.py
@@ -1,18 +1,16 @@
-# -*- coding: utf-8 -*-
 """
-WSGI config for recent project.
+WSGI config for django_chat project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
+
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.settings")
-
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
