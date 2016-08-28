177c6f1a2370571db5a6ac07a4ce1c0203fdcf7e
Joseph Kahihia <info@chrystalsolution.com>
2016-08-28 17:37:23 +0200

    profile view

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -140,7 +140,7 @@ angular
   .run(function ($rootScope, messageService, UserService, $http) {
     // Parse Setup
    // Parse.initialize("moFYNNMeQQJGz74zgDsbaaLtQfNM4hPgMLdYz54M", "0I3OWlWDuZV5udNdosU6xWNBnbJamgyOPApQDK77");
-   UserService.initialize('//127.0.0.1:8000/rest-auth', false);
+   UserService.initialize('http://178.62.41.63:8000/rest-auth', false);
 
 if ($http.defaults.headers.common['Authorization'] != undefined){
 	$rootScope.currentUser = UserService.authPromise;
