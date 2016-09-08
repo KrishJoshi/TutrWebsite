b4322262539435b3e9bd2157044350cb8e16d5a7
Joseph Kahihia <info@chrystalsolution.com>
2016-09-08 16:52:40 +0200

    geo location

--- server/core/serializers.py
+++ server/core/serializers.py
@@ -11,8 +11,8 @@ class UserDetailsView(serializers.ModelSerializer):
         model = BaseUser
         fields = ('id', 'email', 'first_name', 'last_name', 'gender', 'hourrate', 'subjects', 'education', 'degree', 'postcode', 'location', 'name_of_university', 'availability_from', 'availability_to', 'about', 'role', 'avatar')
     def update(self, instance, validated_data):
-        instance.subjects = self.context['request'].data['subjects']
-        print instance.subjects
+        subjects = self.context['request'].data['subjects']
+        print subjects
         return instance
     
 class RegisterSerializer(serializers.Serializer):
